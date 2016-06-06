d1f63c518c0bf5346c0ab528c7e3f24265f85f19
Nick <baihesong@pku.edu.cn>
2016-06-06 21:47:57 +0800

    -1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.get('/tongyi',function(req,res){
 	console.log("show tongyi.html");
-	res.sendFile('/tongyi.html',root:_dirname);
+	res.sendFile('/tongyi.html');
 });
 app.listen(80);
