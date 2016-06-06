92d271f6290772757feb175fcfeaa6ba030ae002
Nick <baihesong@pku.edu.cn>
2016-06-06 22:49:41 +0800

    d

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-	res.sendFile('index.html');
+app.get('/index',function(req,res){
+	res.sendFile('/index.html');
 });
 app.listen(80);
